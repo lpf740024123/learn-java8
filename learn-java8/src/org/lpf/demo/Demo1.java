d30146eb934c9b3b8112733c918a33e0dbd1373d
potato <740024123@qq.com>
2018-04-24 09:12:10 +0800

    io

--- learn-java8/src/org/lpf/demo/Demo1.java
+++ learn-java8/src/org/lpf/demo/Demo1.java
@@ -1,6 +1,11 @@
 package org.lpf.demo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
@@ -14,5 +19,37 @@ public class Demo1 {
 		Function<Integer, Integer> function3 = function.compose(function1);
 		System.out.println(function2.apply(5));
 		System.out.println(function3.apply(5));
+	}
+	@Test
+	public void demo2(){
+		String s = "abc" + "d";
+		String s1 = new String(s);
+		System.out.println(s.equals(s1));
+		System.out.println(s == s1);
+	}
+	public static void main(String[] args) {
+		int a = 0,c = 0;
+		do{
+			c -- ;
+			a = a-1;
+		}while(a>0);
+		System.out.println(c);
+	}
+	@Test	
+	public void demo3(){
+		int[] nums = {10,9,6,20,6,10,8,9,15,80,22,33,11,22,55,44,45,68,68,98,45,47,48,49,1,2,3,4,5};
+		TreeSet tree = new TreeSet();
+		for(int i = 0;i < nums.length ; i++) {
+			tree.add(nums[i]);
+		}
+		System.out.println(tree);
+	}
+	
+	@Test
+	public void demo4(){
+		int[] nums = {10,9,6,20,6,10,8,9,15,80};
+		IntStream stream = Arrays.stream(nums);
+		nums = stream.distinct().sorted().toArray();
+		System.out.println(Arrays.toString(nums));
 	}
 }
--- learn-java8/test/org/lpf/other/Test.java
+++ learn-java8/test/org/lpf/other/Test.java
@@ -2,12 +2,28 @@ package org.lpf.other;
 
 public class Test {
 
-	//a 1 b 2 c 3 d 4
+	public static int t(int a,int b) {
+		if (a >> 31 == -1) 
+			a = ~a + 1;
+		if (b >> 31 == -1)
+			b = ~b + 1;
+		return a ^ b;
+		
+	}
+	
+	public static int t(int a) {
+		if (a % 100 == 0)
+			return a/100;
+		else if (a % 10 == 0) {
+			return a%100*10 + a/100;
+		}
+		return a%10 * 99 + a%100 + a/100;
+	}
+	//a 1 b 2 c 3 d 4 973 300 73 9
 	public static void main(String[] args) {
 
-
-		
-		
-		
+		//System.out.println(Integer.toBinaryString(~-1 ^ 1));
+		System.out.println(Integer.toBinaryString(1000000000));
+		System.out.println(t(300));
 	}
 }
